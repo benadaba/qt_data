d4c2aea2ba98b02479558c610161ec18f123e9c3
Bernard Antwi Adabankah <adabankah.com@gmail.com>
2016-06-14 10:24:13 +0100

    Update lmjp_qt_script.py

--- lmjp_qt_script.py
+++ lmjp_qt_script.py
@@ -35,14 +35,14 @@ np.amax(rv)
 
 #- plot the distribution with matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
+
 plt.hist(rv)
 plt.title("Histogram for Generated Vector")
 plt.xlabel("Value")
 plt.ylabel("Frequency")
 plt.show()
 
-#################################################
+###############################################################################
 '''
 2 - Also, with numpy and python 
 - create a gaussian normal distribution centered at 2 with standard dev of 1 
@@ -50,25 +50,14 @@ plt.show()
 - calculate Percentile 75 for both distributions. 
 Please explain the meaning of such result
 '''
-################################################
+###############################################################################
 #- create a gaussian normal distribution centered at 2 with standard dev of 1 
 mu = 2
 stdv = 1
 gd = np.random.normal(mu, stdv, 50)
 print(gd)
 
-'''
-plt.plot(gd,mlab.normpdf(gd, mu, stdv))
 
-plt.show()
-
-plt.hist(gd, normed=True)
-plt.title("Normal distribution centered at 2 with standard dev of 1")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
-plt.show()
-
-'''
 count, bins, ignored = plt.hist(gd, 10, normed=True)
 plt.title("Normal distribution centered at 2 with standard dev of 1")
 plt.xlabel("Value")
@@ -98,6 +87,7 @@ plt.show()
 # 75th Percentile for gaussian normal distribution
 g75 = np.percentile(gd, 75)
 print("75th Percentile for gaussian normal distribution is:", g75 )
+
 #meaning 
 #This means that 75 percent of the distribution fall below the value of g75
 print("This means that 75 percent of the gaussian normal distribution falls below ", g75)
@@ -105,12 +95,13 @@ print("This means that 75 percent of the gaussian normal distribution falls belo
 # 75th Percentile for uniform distribution between 3 and 4 
 un75 = np.percentile(un34, 75)
 print("75th Percentile for uniform distribution between 3 and 4 is:", un75 )
+
 #meaning 
 #This means that 75 percent of the distribution fall below the value of g75
 print("This means that 75 percent of the uniform distribution between 3 and 4 falls below ", un75)
 
 
-################################################
+###############################################################################
 '''
 3- Calculate MAPE error and MPE error between the two arrays in the attached excel sheet
  - Are these errors representative of the difference/error between the two arrays?
@@ -118,7 +109,7 @@ Please suggest other ways to calculate such errors.
 Also, create a visual representation (plot or web plot) that shows actuals, 
 forecast, errors for each sample and most important statistics (mean, stdev,...)
 '''
-###################
+###############################################################################
 
 from numpy import genfromtxt
 
@@ -130,21 +121,22 @@ act = my_data[4,1:]
 
 #remove all nans
 actuals = act[np.logical_not(np.isnan(act))]
-#actuals =  actuals.astype(int)
+
 #get forecast
 fore = my_data[5,1:]
 
 #remove all nans
 forecast = fore[np.logical_not(np.isnan(fore))]
-#forecast = forecast.astype(int)
+
+
 #Mean absolute percentage error
 MAPE = np.mean((np.abs( forecast != actuals ))) * 100
 
 #Mean percentage error
-MPE = np.mean(( forecast != actuals )) * 100
+MPE = np.mean( forecast != actuals ) * 100
 
 #errors
-errors = np.abs(( actuals - forecast  ))
+errors = np.abs( actuals - forecast  )
 
 # mean , standard deviation, mode and median of actuals and forecasts
 #mean
@@ -170,17 +162,24 @@ f_mode = stats.mode(forecast)
 print("YES these errors are representative of the difference/error between the two arrays")
 
 #Please suggest other ways to calculate such errors.
-#You can calculate writing your own function
-# you can calculate using Excel
+#other ways you can calculate such errors are
+#1- By writing your own function
+#2 for MAPE = ((|Actual - Forecast|)/ |Actual| )* 100
+#3 MAP = ((Actual - Forecast)/ Actual )* 100
 
-plt.figure(figsize=(20,12))
-plt.plot(actuals, label='Actuals', color='red', linewidth=2)
+
+#Also, create a visual representation (plot or web plot) that shows actuals, 
+#forecast, errors for each sample and most important statistics (mean, stdev,...)
+
+plt.figure(figsize=(10,7))
+plt.plot(actuals,  label='Actuals', color='green',  linewidth=2)
 plt.plot(forecast, 
-     '--', color='blue', linewidth=3, label='Forecast')
-#plt.plot(errors, label='Errors', color='green', linewidth=3)
-plt.errorbar( actuals, forecast, errors, linestyle='None', marker='^', label='Errors')
+     '--', color='orange', linewidth=3, label='Forecast')
+xticks_values= range(len(forecast))
+plt.errorbar( xticks_values, forecast, errors, linestyle='None', ecolor="red" , marker='^', label='Errors')
 plt.title("Visualizations of Actuals, Forecasts and Errors")
-#plt.xticks(arange(len(dates))[1::4],dates[1::4], rotation=45)
+plt.xticks(xticks_values)
 plt.legend(loc='upper right')
+plt.xlabel('Units')
 plt.ylabel('Values to be predicted')
 plt.show()
